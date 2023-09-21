ec4ba795f29f6097b74c52d501c92bec32998e0a
ayee-lucas <nouminosxz@gmail.com>
2023-09-21 14:34:24 -0600

    rebranded

--- src/app/admin/tests/app.component.spec.ts
+++ src/app/admin/tests/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'inv-portal'`, () => {
+  it(`should have as title 'sip-portal'`, () => {
     const fixture = TestBed.createComponent(AdminComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('inv-portal');
+    expect(app.title).toEqual('sip-portal');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'inv-portal app is running!'
+      'sip-portal app is running!'
     );
   });
 });
