67c48a74edf01c044e829a7dfe83b55c9ca236cf
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 01:08:05 -0600

    renamed

---
+++ src/app/operation/components/operation.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OperationComponent } from './operation.component';
+
+describe('ComponentsComponent', () => {
+  let component: OperationComponent;
+  let fixture: ComponentFixture<OperationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OperationComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(OperationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/operation/components/operation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './operation.component.html',
+  styleUrls: ['./operation.component.scss']
+})
+export class OperationComponent {}
