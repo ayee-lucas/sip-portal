15a3d8de13b30b5698c9a6028e1f950fbd0116ea
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 01:14:30 -0600

    first glance component initiation

---
+++ src/app/admin/components/first-glance/first-glance.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-glance',
+  templateUrl: './first-glance.component.html',
+  styleUrls: ['./first-glance.component.scss']
+})
+export class FirstGlanceComponent {}
