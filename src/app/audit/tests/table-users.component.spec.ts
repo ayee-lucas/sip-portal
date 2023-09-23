bb0ac35763a11f58bdd9297312ca352a61bfc77a
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 01:03:13 -0600

    Generic angular cli test

--- src/app/audit/tests/table-users.component.spec.ts
+++ src/app/audit/tests/table-users.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableUsersComponent } from './table-users.component';
+import { TableUsersComponent } from '../components/table-users/table-users.component';
 
 describe('TableUsersComponent', () => {
   let component: TableUsersComponent;
