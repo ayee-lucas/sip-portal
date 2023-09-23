88d8c409f6987b1889bfe50184a666a2b635ddd4
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 01:31:15 -0600

    Operation module + Component added

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OperationComponent } from './components/operation.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: OperationComponent }];
 
 @NgModule({
   declarations: [OperationComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  bootstrap: [OperationComponent]
 })
 export class OperationModule {}
