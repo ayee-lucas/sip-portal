7b43d5baa4e5830a407288b5b2f91b557b336ff0
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 01:19:04 -0600

    Operation files init

---
+++ src/app/operation/operation.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OperationComponent } from './components/operation.component';
+
+@NgModule({
+  declarations: [OperationComponent],
+  imports: [CommonModule]
+})
+export class OperationModule {}
