05ee723abde3ca5f131a90729596bfe4a25557e5
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 00:52:18 -0600

    Declared components | Imported required modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AdminRoutingModule } from './admin-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminComponent } from './components/admin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FirstGlanceComponent } from './components/first-glance/first-glance.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent, SidebarComponent, FirstGlanceComponent],
   imports: [
     BrowserModule,
     AdminRoutingModule,
     BrowserAnimationsModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule
   ],
   bootstrap: [AdminComponent]
 })
