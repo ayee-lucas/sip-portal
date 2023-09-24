1b9f26f21db702711a9847d06f0214a85b79bd93
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 22:15:14 -0600

    Added user selector for operation

---
+++ src/app/operation/components/user-selector/user-selector.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-selector',
+  templateUrl: './user-selector.component.html',
+  styleUrls: ['./user-selector.component.scss']
+})
+export class UserSelectorComponent {}
