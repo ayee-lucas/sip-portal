9ad696a4749c2145f40df0fd0c159e08f915d77d
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 22:14:44 -0600

    Improved active tab style handling

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -5,23 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
-  index!: number | null;
-
-  constructor() {
-    if (!localStorage.getItem('index')) {
-      this.index = 0;
-      localStorage.setItem('index', this.index.toString());
-
-      return;
-    }
-
-    this.index = Number(localStorage.getItem('index'));
-    console.log(this.index);
-  }
-
-  changeIndex(index: number) {
-    localStorage.setItem('index', index.toString());
-    this.index = index;
-  }
-}
+export class SidebarComponent {}
