0d3dbdbc8c232a0574708739b88e0729a828f14c
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 22:13:48 -0600

    Declared user selector tab

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OperationComponent } from './components/operation.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UserSelectorComponent } from './components/user-selector/user-selector.component';
 
 const routes: Routes = [{ path: '', component: OperationComponent }];
 
 @NgModule({
-  declarations: [OperationComponent],
+  declarations: [OperationComponent, UserSelectorComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
   bootstrap: [OperationComponent]
 })
