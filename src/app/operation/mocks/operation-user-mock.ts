c598f1300dd29dc8580ad348e5fd422744c661a5
ayee-lucas <nouminosxz@gmail.com>
2023-09-23 22:14:03 -0600

    mock data for operation

---
+++ src/app/operation/mocks/operation-user-mock.ts
@@ -0,0 +1,123 @@
+export const operationUserMock = {
+  content: [
+    {
+      id: 17,
+      email: 'advasquez@diunsa.hn',
+      name: 'Ada Vasquez',
+      status: true,
+      profile: {
+        id: 57,
+        name: 'QA-Proveedor',
+        description: 'QA Perfil proveedor',
+        providerProfile: true,
+        status: true
+      }
+    },
+    {
+      id: 3,
+      email: 'apitrade@diunsa.hn',
+      name: 'Api Trade',
+      status: true,
+      profile: {
+        id: 1,
+        name: 'PRF - Administrador General',
+        description: 'Perfil para administradores',
+        providerProfile: false,
+        status: true
+      }
+    },
+    {
+      id: 18,
+      email: 'hernandez@is4tech.com',
+      name: 'Ariz Hernandez e',
+      status: true,
+      profile: {
+        id: 1,
+        name: 'PRF - Administrador General',
+        description: 'Perfil para administradores',
+        providerProfile: false,
+        status: true
+      }
+    },
+    {
+      id: 26,
+      email: 'csagastume@diunsa.hn',
+      name: 'Carlos Edgardo Sagastume Chavez',
+      status: true,
+      profile: {
+        id: 1,
+        name: 'PRF - Administrador General',
+        description: 'Perfil para administradores',
+        providerProfile: false,
+        status: true
+      }
+    },
+    {
+      id: 20,
+      email: 'cherrera@diunsa.hn',
+      name: 'Christopher Herrera',
+      status: true,
+      profile: {
+        id: 1,
+        name: 'PRF - Administrador General',
+        description: 'Perfil para administradores',
+        providerProfile: false,
+        status: true
+      }
+    },
+    {
+      id: 15,
+      email: 'drosales@diunsa.hn',
+      name: 'Denis Rosales',
+      status: true,
+      profile: {
+        id: 40,
+        name: 'PRF - Proveedores',
+        description: 'Perfil para usuarios de proveedor',
+        providerProfile: true,
+        status: true
+      }
+    },
+    {
+      id: 9,
+      email: 'esabillon@coffeelandhn.com',
+      name: 'Edwin Sabillon',
+      status: true,
+      profile: {
+        id: 40,
+        name: 'PRF - Proveedores',
+        description: 'Perfil para usuarios de proveedor',
+        providerProfile: true,
+        status: true
+      }
+    },
+    {
+      id: 29,
+      email: 'ecoronado@is4tech.com',
+      name: 'Eliezer Coronado',
+      status: false,
+      profile: {
+        id: 59,
+        name: 'Test Role',
+        description: 'Desc',
+        providerProfile: false,
+        status: true
+      }
+    },
+    {
+      id: 5,
+      email: 'esramirez@diunsa.hn',
+      name: 'Estuardo Ramirez',
+      status: true,
+      profile: {
+        id: 1,
+        name: 'PRF - Administrador General',
+        description: 'Perfil para administradores',
+        providerProfile: false,
+        status: true
+      }
+    }
+  ],
+  totalElements: 20,
+  totalPages: 2
+};
