b4f78dd7504890adf42be21295a6b5f5f2e6a7d4
ayee-lucas <nouminosxz@gmail.com>
2023-09-24 22:11:09 -0600

    User design imporved

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { OperationComponent } from './components/operation.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserSelectorComponent } from './components/user-selector/user-selector.component';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [{ path: '', component: OperationComponent }];
 
 @NgModule({
   declarations: [OperationComponent, UserSelectorComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatIconModule],
   bootstrap: [OperationComponent]
 })
 export class OperationModule {}
