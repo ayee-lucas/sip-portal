4e58447141ec73c09db0d9014050ac339f27e47a
ayee-lucas <nouminosxz@gmail.com>
2023-09-24 21:56:36 -0600

    Added client subscription to userservice

--- src/app/operation/components/user-selector/user-selector.component.ts
+++ src/app/operation/components/user-selector/user-selector.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserOperationService } from '../../services/user-operation.service';
+import { Observable } from 'rxjs';
+import { ResponseOperationUser } from '../../types/response-type-operation';
 
 @Component({
   selector: 'app-user-selector',
   templateUrl: './user-selector.component.html',
   styleUrls: ['./user-selector.component.scss']
 })
-export class UserSelectorComponent {}
+export class UserSelectorComponent implements OnInit {
+  response$!: Observable<ResponseOperationUser[]>;
+
+  constructor(private operationService: UserOperationService) {}
+
+  ngOnInit(): void {
+    this.response$ = this.operationService.getUsers();
+
+    this.operationService.init();
+  }
+}
