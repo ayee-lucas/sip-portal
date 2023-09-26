d1e813921782d50c889bf6126bdff1d838265232
cramos-2018521 <cramos-2018521@kinal.edu.gt>
2023-09-26 10:36:39 -0600

    Validations added

--- src/app/operation/components/user-selector/user-selector.component.ts
+++ src/app/operation/components/user-selector/user-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import FormBuilder and Validators
 import { UserOperationService } from '../../services/user-operation.service';
 import { Observable } from 'rxjs';
 import { ResponseOperationUser } from '../../types/response-type-operation';
@@ -10,12 +11,26 @@ import { ResponseOperationUser } from '../../types/response-type-operation';
 })
 export class UserSelectorComponent implements OnInit {
   response$!: Observable<ResponseOperationUser[]>;
+  searchForm: FormGroup;
 
-  constructor(private operationService: UserOperationService) {}
+  constructor(
+    private operationService: UserOperationService,
+    private formBuilder: FormBuilder
+  ) {
+    this.searchForm = this.formBuilder.group({
+      search: ['', [Validators.required]] // Add a required validator
+    });
+  }
 
   ngOnInit(): void {
     this.response$ = this.operationService.getUsers();
-
     this.operationService.init();
+  }
+
+  onSubmit() {
+    if (this.searchForm.valid) {
+      const searchValue = this.searchForm.value.search;
+      console.log('Searching for:', searchValue);
+    }
   }
 }
