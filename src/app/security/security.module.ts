266dcf1e80bf677233a25797b49670a6114d18b6
ayee-lucas <nouminosxz@gmail.com>
2023-09-27 00:16:31 -0600

    Added route object

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { environment } from '../../environments/environment.development';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: environment.ROUTE.login,
+    pathMatch: 'full'
+  },
+  { path: environment.ROUTE.login, component: LoginComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [CommonModule]
 })
 export class SecurityModule {}
