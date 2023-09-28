d5a7207e31c84a412f3c8c80ac526f4ed234d124
ayee-lucas <nouminosxz@gmail.com>
2023-09-27 21:08:35 -0600

    Added logged in state mock handler to hide elements and show login page

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { environment } from '../../environments/environment.development';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: environment.ROUTE.login,
-    pathMatch: 'full'
-  },
-  { path: environment.ROUTE.login, component: LoginComponent }
+    component: LoginComponent,
+    outlet: 'security'
+  }
 ];
 
 @NgModule({
