cfb5caa3b180ded840cad662b365ca1720d1c86a
ayee-lucas <nouminosxz@gmail.com>
2023-09-28 16:24:30 -0600

    Added authservice for logginState handling

--- src/app/security/services/auth-status.service.ts
+++ src/app/security/services/auth-status.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthStatusService {}
+export class AuthStatusService {
+  private isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+  setAuthStatus(status: boolean) {
+    this.isLoggedIn$.next(status);
+  }
+
+  authStatus(): Observable<boolean> {
+    return this.isLoggedIn$;
+  }
+}
