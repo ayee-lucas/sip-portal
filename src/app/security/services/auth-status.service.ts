6a906842ce0658f1103a60368e93d3336fe41c38
ayee-lucas <nouminosxz@gmail.com>
2023-09-29 11:17:05 -0600

    Check if expiration date exists in cookie storage

--- src/app/security/services/auth-status.service.ts
+++ src/app/security/services/auth-status.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +8,19 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class AuthStatusService {
   private isLoggedIn$ = new BehaviorSubject<boolean>(false);
 
+  constructor(private cookieService: CookieService) {}
+
   setAuthStatus(status: boolean) {
     this.isLoggedIn$.next(status);
   }
 
   authStatus(): Observable<boolean> {
+    const expiration = this.cookieService.get('expiration');
+
+    if (!expiration) {
+      this.cookieService.deleteAll();
+    }
+
     return this.isLoggedIn$;
   }
 }
