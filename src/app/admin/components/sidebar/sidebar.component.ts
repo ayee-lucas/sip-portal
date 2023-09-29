1d500966fb90aaaaf079e57abd1823949694b14a
ayee-lucas <nouminosxz@gmail.com>
2023-09-29 16:15:16 -0600

    Service injection

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthStatusService } from '../../../security/services/auth-status.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private authStatusService: AuthStatusService) {}
+}
