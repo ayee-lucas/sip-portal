3d4bf4d48b5aa250464728fb428b075c1641837d
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2023-09-29 21:28:08 -0600

    Type para mock audit

--- src/app/audit/types/audit-form.types.ts
+++ src/app/audit/types/audit-form.types.ts
@@ -1,5 +1,28 @@
 import { FormControl } from '@angular/forms';
 
+export type User = {
+  identifier: string;
+  date: string;
+  entity: string;
+  user: string;
+  operation: string;
+  id: number;
+};
+
+export type Audit = {
+  content: User[];
+  page: number;
+  totalElements: number;
+  totalPages: number;
+};
+
+export const mockAudit: Audit = {
+  content: [],
+  page: 0,
+  totalElements: 30,
+  totalPages: 2
+};
+
 export type AuditForm = {
   identifier: FormControl<string | null>;
   start: FormControl<Date | null>;
