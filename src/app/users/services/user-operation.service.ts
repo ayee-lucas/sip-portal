50820bff07b8625900bc3c0e21170b122395a100
ayee-lucas <nouminosxz@gmail.com>
2023-10-03 17:08:09 -0600

    Removed temporal service handler

--- src/app/users/services/user-operation.service.ts
+++ src/app/users/services/user-operation.service.ts
@@ -1,39 +1,18 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable } from 'rxjs';
-import {
-  ResponseOperation,
-  ResponseOperationError,
-  ResponseOperationUser
-} from '../types/response-type-operation';
-import { operationUserMock } from '../mocks/operation-user-mock';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ResponseUser } from '../types/response-type-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserOperationService {
-  private userResponse$ = new BehaviorSubject<
-    ResponseOperation | ResponseOperationError
-  >({ error: 'No users yet' });
+  private userResponse$ = new BehaviorSubject<ResponseUser>([]);
 
   init() {
-    // This BehaviorSubject will be used to mock the data coming from the API
-    // Delete this BehaviorSubject when api is ready
-    const mockData = new BehaviorSubject(operationUserMock);
-
-    mockData.subscribe(data => {
-      this.userResponse$.next(data);
-    });
+    return this.userResponse$;
   }
 
-  getUsers(): Observable<ResponseOperationUser[]> {
-    return this.userResponse$.pipe(
-      map(res => {
-        if ('error' in res) {
-          return [];
-        }
-
-        return res.content;
-      })
-    );
+  getUsers(): Observable<ResponseUser> {
+    return this.userResponse$;
   }
 }
