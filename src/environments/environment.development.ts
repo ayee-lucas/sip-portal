36d88917847d3aa01d211304b60f832a641a142b
ayee-lucas <nouminosxz@gmail.com>
2023-10-03 23:51:44 -0600

    Added Backend path in environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,16 @@
+const ROOT_PATH = 'http://localhost:8082';
+const PORTAL_PREFIX = '/sip-services';
+const API_PREFIX = '/api';
+const USER_PREFIX = '/user';
+
 export const environment = {
   ROUTE: {
     audit: 'audit',
     users: 'users',
     security: 'auth',
     login: 'login'
+  },
+  SERVER_PATH: {
+    GET_USERS: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + USER_PREFIX
   }
 };
