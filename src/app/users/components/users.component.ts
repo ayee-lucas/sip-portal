9194f7f019871a7da3ae502f936531440c3ac21c
ayee-lucas <nouminosxz@gmail.com>
2023-10-04 10:00:32 -0600

    Added page params

--- src/app/users/components/users.component.ts
+++ src/app/users/components/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserOperationService } from '../services/user-operation.service';
 import { map, Observable } from 'rxjs';
 import { ResponseUserSuccess } from '../types/response-type-users';
+import { AuditQueryService } from '../../query/services/audit-query.service';
 
 @Component({
   selector: 'app-components',
@@ -10,8 +11,16 @@ import { ResponseUserSuccess } from '../types/response-type-users';
 })
 export class UsersComponent implements OnInit {
   responseUser$!: Observable<ResponseUserSuccess>;
+  private page: number;
 
-  constructor(private userOperationService: UserOperationService) {}
+  constructor(
+    private userOperationService: UserOperationService,
+    private queryService: AuditQueryService
+  ) {
+    const params = this.queryService.getParams();
+
+    this.page = Number(params['params'].page);
+  }
 
   ngOnInit() {
     this.responseUser$ = this.userOperationService.getUsers().pipe(
@@ -33,6 +42,21 @@ export class UsersComponent implements OnInit {
 
   refresh() {
     this.userOperationService.refresh();
+  }
+
+  nextPage() {
+    this.queryService.updateParams({ page: this.page++ });
+
+    this.userOperationService.refresh();
+
     this.userOperationService.init();
+  }
+
+  previousPage() {
+    this.queryService.updateParams({ page: this.page-- });
+
+    this.userOperationService.refresh();
+
+    console.log(this.page);
   }
 }
