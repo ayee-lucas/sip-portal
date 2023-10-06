508235d9f152a2fff6f4a0e51f59395d7cdfe56e
ayee-lucas <nouminosxz@gmail.com>
2023-10-06 00:11:28 -0600

    Service to handle Selected User state request to the backend by idd

---
+++ src/app/users/test/user-selector-search.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UserSelectorSearchService } from '../services/user-selector-search.service';
+
+describe('UserSelectorSearchService', () => {
+  let service: UserSelectorSearchService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UserSelectorSearchService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
