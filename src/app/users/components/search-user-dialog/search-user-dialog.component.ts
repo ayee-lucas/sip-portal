383a56f80344c9aecb005e0c9ad92a16aa59b1e0
ayee-lucas <nouminosxz@gmail.com>
2023-10-06 00:05:41 -0600

    Dialog component that request by id the user

---
+++ src/app/users/components/search-user-dialog/search-user-dialog.component.ts
@@ -0,0 +1,48 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AuditQueryService } from '../../../query/services/audit-query.service';
+import { UserSelectorSearchService } from '../../services/user-selector-search.service';
+import { map, Observable } from 'rxjs';
+import { ResponseUserError, User } from '../../types/response-type-users';
+import { _isUser } from '../../../shared/utils/TypeGuards';
+
+type SearchUserDialogComponentData = {
+  search: any;
+};
+
+@Component({
+  selector: 'app-search-user-dialog',
+  templateUrl: './search-user-dialog.component.html',
+  styleUrls: ['./search-user-dialog.component.scss']
+})
+export class SearchUserDialogComponent implements OnInit {
+  user$!: Observable<User | ResponseUserError>;
+  /** @internal */ isUser = _isUser;
+
+  constructor(
+    public dialogRef: MatDialogRef<SearchUserDialogComponent>,
+    private queryService: AuditQueryService,
+    private userSearchService: UserSelectorSearchService,
+    @Inject(MAT_DIALOG_DATA) public data: SearchUserDialogComponentData
+  ) {}
+
+  ngOnInit() {
+    this.requestUser();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  private requestUser() {
+    const idParam = this.queryService.getParams()['params'].id;
+
+    this.user$ = this.userSearchService.getUser().pipe(
+      map(data => {
+        return data as User | ResponseUserError;
+      })
+    );
+
+    this.userSearchService.init(idParam);
+  }
+}
