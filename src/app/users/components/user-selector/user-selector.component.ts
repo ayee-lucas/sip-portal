78287d1d1ac945320d274af40f59ddf250c7be61
ayee-lucas <nouminosxz@gmail.com>
2023-10-06 23:40:23 -0600

    Removed param handling (since is handled by service now)

--- src/app/users/components/user-selector/user-selector.component.ts
+++ src/app/users/components/user-selector/user-selector.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../types/response-type-users';
-import { AuditQueryService } from '../../../query/services/audit-query.service';
 import { Observable } from 'rxjs';
 import { SelectedUserService } from '../../services/selected-user.service';
 
@@ -14,10 +13,7 @@ export class UserSelectorComponent implements OnInit {
 
   userSelected$!: Observable<User | null>;
 
-  constructor(
-    private queryService: AuditQueryService,
-    private selectedUserService: SelectedUserService
-  ) {}
+  constructor(private selectedUserService: SelectedUserService) {}
 
   ngOnInit() {
     this.userSelected$ = this.selectedUserService.getUserSelected();
@@ -28,13 +24,6 @@ export class UserSelectorComponent implements OnInit {
   }
 
   selectUser(user: User) {
-    this.queryService.updateParams({
-      selected: user.userId,
-      name: user.names,
-      lastName: user.lastNames,
-      email: user.email,
-      status: user.status
-    });
     this.selectedUserService.setUserSelected(user);
   }
 }
