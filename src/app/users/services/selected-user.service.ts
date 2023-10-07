68c58cdd1717cc125420a1709a3e6905690b1d83
ayee-lucas <nouminosxz@gmail.com>
2023-10-06 23:33:39 -0600

    Moved query handling to the selected-user.service.ts instead of handling manually when needed

--- src/app/users/services/selected-user.service.ts
+++ src/app/users/services/selected-user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../types/response-type-users';
+import { AuditQueryService } from '../../query/services/audit-query.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,24 @@ import { User } from '../types/response-type-users';
 export class SelectedUserService {
   private userSelected$ = new BehaviorSubject<User | null>(null);
 
+  constructor(private queryService: AuditQueryService) {}
+
   getUserSelected(): Observable<User | null> {
     return this.userSelected$;
   }
 
-  setUserSelected(user: User | null) {
+  setUserSelected(user: User) {
+    if (user) {
+      this.queryService.updateParams({
+        selected: user.userId,
+        name: user.names,
+        lastName: user.lastNames,
+        email: user.email,
+        status: user.status,
+        profile: user.profileId
+      });
+    }
+
     this.userSelected$.next(user);
   }
 }
