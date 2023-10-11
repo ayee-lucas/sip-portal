6b853009c12a353b27866e08f215451720e0e133
Alan Lopez <nouminosxz@gmail.com>
2023-10-11 15:46:33 -0700

    User search dialog param handling

---
+++ src/app/admin/components/profiles/search-profile-dialog/search-profile-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { QueryService } from '../../../../query/services/query.service';
+import {
+  ResponseError,
+  ResponseLoading
+} from '../../../types/response-type-users';
+import { Observable } from 'rxjs';
+import { Profile } from '../../../types/response-type-profiles';
+import { ProfileSelectorSearchService } from '../../../services/profile-selector-search.service';
+
+@Component({
+  selector: 'app-search-profile-dialog',
+  templateUrl: './search-profile-dialog.component.html'
+})
+export class SearchProfileDialogComponent implements OnInit {
+  profile$!: Observable<Profile | ResponseError | ResponseLoading>;
+
+  constructor(
+    public dialogRef: MatDialogRef<SearchProfileDialogComponent>,
+    private queryService: QueryService,
+    private profileSelectorSearchService: ProfileSelectorSearchService
+  ) {}
+
+  ngOnInit() {
+    this.requestProfile();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  private requestProfile() {
+    const idParam = this.queryService.getParams()['params'].id;
+
+    this.profile$ = this.profileSelectorSearchService.getProfile();
+
+    this.profileSelectorSearchService.init(idParam);
+  }
+}
