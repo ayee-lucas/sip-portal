44806048ee9d104e723b08774ebb76acfe958469
Alan Lopez <nouminosxz@gmail.com>
2023-10-10 18:17:08 -0700

    Cli generated

---
+++ src/app/admin/test/profiles.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilesComponent } from '../components/profiles/profiles.component';
+
+describe('ProfilesComponent', () => {
+  let component: ProfilesComponent;
+  let fixture: ComponentFixture<ProfilesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProfilesComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProfilesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
