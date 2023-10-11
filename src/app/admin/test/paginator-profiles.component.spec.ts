c2a7080b7e2e8a9caf25ea40c200074156273525
Alan Lopez <nouminosxz@gmail.com>
2023-10-11 11:57:15 -0700

    Paginator for profile selector

---
+++ src/app/admin/test/paginator-profiles.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaginatorProfilesComponent } from '../components/paginator-profiles/paginator-profiles.component';
+
+describe('PaginatorProfilesComponent', () => {
+  let component: PaginatorProfilesComponent;
+  let fixture: ComponentFixture<PaginatorProfilesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PaginatorProfilesComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PaginatorProfilesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
