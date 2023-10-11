c151dd8688e6485dd9548cf3d7f0a5c871a24f77
Alan Lopez <nouminosxz@gmail.com>
2023-10-10 18:16:12 -0700

    Added Profile Success type guard

--- src/app/shared/utils/TypeGuards.ts
+++ src/app/shared/utils/TypeGuards.ts
@@ -1,15 +1,27 @@
-import { ResponseUserError, User } from '../../admin/types/response-type-users';
+import { ResponseError, User } from '../../admin/types/response-type-users';
+import {
+  ResponseProfile,
+  ResponseProfileSuccess
+} from '../../admin/types/response-type-profiles';
 
-export function _isUser(t: User | ResponseUserError): t is User {
+export function _isUser(t: User | ResponseError): t is User {
   if ('error' in t) return false;
 
   return t !== undefined && 'userId' in t;
 }
 
-export function _isResponseUserError(
-  t: User | ResponseUserError
-): t is ResponseUserError {
+export function _isResponseError(t: User | ResponseError): t is ResponseError {
   if ('error' in t) return true;
 
   return false;
+}
+
+export function _IsProfileSuccess(
+  t: ResponseProfile
+): t is ResponseProfileSuccess {
+  if (!t) return false;
+
+  if ('error' in t) return false;
+
+  return !('loading' in t);
 }
