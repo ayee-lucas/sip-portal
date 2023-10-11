e89fd8bffaa50c7fec92f8ad6a1924340a056d39
Alan Lopez <nouminosxz@gmail.com>
2023-10-10 18:18:17 -0700

    Added service for getting Profiles

---
+++ src/app/admin/components/profiles/profiles.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ResponseProfile } from '../../types/response-type-profiles';
+import { Observable } from 'rxjs';
+import { ProfileRequestService } from '../../services/profile-request.service';
+import { QueryService } from '../../../query/services/query.service';
+import { _IsProfileSuccess } from '../../../shared/utils/TypeGuards';
+
+@Component({
+  selector: 'app-profiles',
+  templateUrl: './profiles.component.html'
+})
+export class ProfilesComponent implements OnInit {
+  /** @internal */ isProfileSuccess = _IsProfileSuccess;
+
+  profiles$!: Observable<ResponseProfile>;
+
+  constructor(
+    private profileRequest: ProfileRequestService,
+    private queryService: QueryService
+  ) {}
+
+  ngOnInit() {
+    const params = this.queryService.getParams();
+
+    this.profiles$ = this.profileRequest.getProfiles();
+
+    this.profileRequest.init(params);
+  }
+}
---
+++ src/app/admin/services/profile-request.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, of } from 'rxjs';
+import { ResponseProfile } from '../types/response-type-profiles';
+import { HttpClient } from '@angular/common/http';
+import { QueryService } from '../../query/services/query.service';
+import { Params } from '@angular/router';
+import { environment } from '../../../environments/environment.development';
+import { ResponseError } from '../types/response-type-users';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileRequestService {
+  private profiles$ = new BehaviorSubject<ResponseProfile>({ loading: true });
+
+  constructor(
+    private http: HttpClient,
+    private queryService: QueryService,
+    private meessageService: MessageService
+  ) {}
+
+  init(params: Params) {
+    const url = new URL(environment.SERVER_PATH.PROFILES);
+
+    Object.keys(params).forEach(key => {
+      url.searchParams.append(key, params[key]);
+    });
+
+    this.requestProfiles(url.toString());
+  }
+
+  getProfiles(): Observable<ResponseProfile> {
+    return this.profiles$;
+  }
+
+  private requestProfiles(url: string) {
+    console.log(url);
+    this.http
+      .get<ResponseProfile>(url)
+      .pipe(
+        catchError(err => {
+          if ('error' in err) {
+            return of(err.error);
+          }
+
+          const data: ResponseError = {
+            error: {
+              errorCode: 0,
+              errorType: 'FATAL_ERROR',
+              code: 'FETCH_DATA_ERROR',
+              description: 'Failed to retrieve data'
+            }
+          };
+
+          return of(data);
+        })
+      )
+      .subscribe(data => {
+        if ('error' in data) {
+          this.meessageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: data.error.description
+          });
+
+          this.profiles$.next(data);
+
+          return;
+        }
+
+        this.profiles$.next(data);
+      });
+  }
+}
