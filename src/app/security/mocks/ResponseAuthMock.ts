ed036cc7cabaf75080e5c1deed53bc4ed264dad6
cramos-2018521 <cramos-2018521@kinal.edu.gt>
2023-10-11 15:03:17 -0600

    Comment changed from Spanish to English

--- src/app/security/mocks/ResponseAuthMock.ts
+++ src/app/security/mocks/ResponseAuthMock.ts
@@ -3,6 +3,6 @@ import { AuthResponseType } from '../types/AuthResponseType';
 export const ResponseAuthMock: AuthResponseType = {
   token:
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzY29wZSI6WyJvcGVuaWQiXSwidXNlcl9uYW1lIjoicm9vdEBpczR0ZWNo.lmVBtS7Afy9k1dGFgwHVKJF-vfJQomQ-WGZO4ojQkJ65G7fj2X7pyWpsWEdVg-KRbI',
-  expiration: 1699679999000, // 31 de diciembre de 2023, 11:59:59 PM
+  expiration: 1699679999000, // 31 de diciembre de 2023, 23:59:59
   authorities: ['ROLE_QRY_AUDIT', 'ROLE_USER', 'ROLE_PROFILE']
 };
