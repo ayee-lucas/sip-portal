95e4333dcc22dd722f80341094004362d8c8fd76
Alan Lopez <nouminosxz@gmail.com>
2023-10-13 12:22:46 -0700

    Added profile service for setting and getting the selected profile

--- src/app/admin/services/profile-selector.service.ts
+++ src/app/admin/services/profile-selector.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Profile } from '../types/response-type-profiles';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { QueryService } from '../../query/services/query.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileSelectorService {
   private profileSelected$ = new BehaviorSubject<Profile | null>(null);
+
+  constructor(private queryService: QueryService) {}
+
+  getProfile(): Observable<Profile | null> {
+    return this.profileSelected$;
+  }
+
+  setSelectedProfile(profile: Profile) {
+    if (profile) {
+      this.queryService.updateParams({
+        selected: profile.profileId,
+        name: profile.name,
+        description: profile.description_profile,
+        status: profile.status
+      });
+    }
+
+    this.profileSelected$.next(profile);
+  }
 }
