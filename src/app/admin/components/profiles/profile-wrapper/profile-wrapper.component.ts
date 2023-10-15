a879715d35216245b32d75382a343c1146cadcf9
Alan Lopez <nouminosxz@gmail.com>
2023-10-15 01:19:55 -0600

    removed subscription since now the request is handled by the selector service

--- src/app/admin/components/profiles/profile-wrapper/profile-wrapper.component.ts
+++ src/app/admin/components/profiles/profile-wrapper/profile-wrapper.component.ts
@@ -1,32 +1,19 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ProfileSelectorService } from '../../../services/profiles/profile-selector.service';
-import { Observable, Subject, takeUntil } from 'rxjs';
-import { Profile } from '../../../types/response-type-profiles';
 import { QueryService } from '../../../../query/services/query.service';
-import { ProfileSelectorSearchService } from '../../../services/profiles/profile-selector-search.service';
 import { Params } from '@angular/router';
-import { _isProfile } from '../../../../shared/utils/TypeGuards';
+import { ProfileSelectorService } from '../../../services/profiles/profile-selector.service';
+import { Observable } from 'rxjs';
+import { Profile } from '../../../types/response-type-profiles';
 
 @Component({
   selector: 'app-profile-wrapper',
   templateUrl: './profile-wrapper.component.html'
 })
 export class ProfileWrapperComponent implements OnInit, OnDestroy {
-  // Profile selected observable to be used in the template
-  profileSelected$!: Observable<Profile | null>;
-
   // Params object to be used in the ngOnInit method
   params: Params;
 
-  // Unsubscribe subject to be used in the ngOnDestroy method
-  private unsubscribe$ = new Subject<boolean>();
-
-  // Type guard to check if the object is a Profile
-  /**
-   * @private
-   * @internal
-   */
-  private isProfile = _isProfile;
+  selectedProfile$!: Observable<Profile | null>;
 
   // Injecting the services to be used in the component
 
@@ -35,14 +22,12 @@ export class ProfileWrapperComponent implements OnInit, OnDestroy {
    * it uses the queryService to get the params object from the URL
    * assigns the params object to the params property
    *
-   * @param profileSelectorService
-   * @param profileSelectorSearchService
    * @param queryService
+   * @param profileSelectorService
    */
   constructor(
-    private profileSelectorService: ProfileSelectorService,
-    private profileSelectorSearchService: ProfileSelectorSearchService,
-    private queryService: QueryService
+    private queryService: QueryService,
+    private profileSelectorService: ProfileSelectorService
   ) {
     this.params = this.queryService.getParams();
   }
@@ -58,52 +43,29 @@ export class ProfileWrapperComponent implements OnInit, OnDestroy {
    * @memberof ProfileWrapperComponent
    */
   ngOnInit() {
+    this.selectedProfile$ = this.profileSelectorService.getProfile();
+
     const selectParam = this.params['params'].selected;
 
     if (selectParam && selectParam > 0) {
-      this.requestProfile();
-
-      this.profileSelectorSearchService.init(selectParam);
-
+      this.profileSelectorService.setSelectedProfile(selectParam);
       return;
     }
-
-    this.profileSelected$ = this.profileSelectorService.getProfile();
   }
 
-  // OnDestroy method
-
-  /**
-   * This method is called when the component is destroyed
-   * Handles the unsubscription from the profileSelected$ observable
-   * @returns void
-   * @memberof ProfileWrapperComponent
-   */
   ngOnDestroy() {
-    this.unsubscribe$.next(true);
-    this.unsubscribe$.complete();
+    this.profileSelectorService.clearProfile();
   }
 
-  // Request profile method
+  toggleAddProfile() {
+    const params = this.queryService.getParams();
 
-  /**
-   * This method is called when the component is initialized
-   * Calls the getProfile method from the profileSelectorSearchService
-   * to fetch the profile data from the API when the component is initialized
-   * @private
-   * @returns void
-   * @memberof ProfileWrapperComponent
-   */
-  private requestProfile() {
-    this.profileSelectorSearchService
-      .getProfile()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(data => {
-        if (this.isProfile(data)) {
-          this.profileSelectorService.setSelectedProfile(data);
+    this.queryService.setParams({
+      page: params['params'].page,
+      size: params['params'].size,
+      sort: params['params'].sort
+    });
 
-          this.profileSelected$ = this.profileSelectorService.getProfile();
-        }
-      });
+    this.profileSelectorService.clearProfile();
   }
 }
