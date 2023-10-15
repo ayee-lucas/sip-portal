f878fe4c07566771d2ddce9258150d286340f0b4
Alan Lopez <nouminosxz@gmail.com>
2023-10-15 01:16:28 -0600

    Profile selector service now handles http request

--- src/app/admin/services/profiles/profile-selector.service.ts
+++ src/app/admin/services/profiles/profile-selector.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Profile } from '../../types/response-type-profiles';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { QueryService } from '../../../query/services/query.service';
+import { ProfileSelectorSearchService } from './profile-selector-search.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +10,59 @@ import { QueryService } from '../../../query/services/query.service';
 export class ProfileSelectorService {
   private profileSelected$ = new BehaviorSubject<Profile | null>(null);
 
-  constructor(private queryService: QueryService) {}
+  constructor(
+    private queryService: QueryService,
+    private profileSelectorSearchService: ProfileSelectorSearchService
+  ) {}
 
   getProfile(): Observable<Profile | null> {
     return this.profileSelected$;
   }
 
-  setSelectedProfile(profile: Profile) {
-    if (profile) {
-      this.queryService.updateParams({
-        selected: profile.profileId,
-        name: profile.name,
-        description: profile.description_profile,
-        status: profile.status
-      });
+  clearProfile() {
+    this.profileSelected$.next(null);
+  }
+
+  setSelectedProfile(id: number) {
+    if (!id) {
+      return;
     }
 
-    this.profileSelected$.next(profile);
+    this.profileSelectorSearchService
+      .getProfile()
+      .pipe(
+        map(res => {
+          if ('error' in res) {
+            console.log('error', res);
+          }
+
+          if ('loading' in res) {
+            console.log('loading', res);
+          }
+
+          return res as Profile | null;
+        })
+      )
+      .subscribe(data => {
+        if (data) {
+          this.queryService.updateParams({
+            selected: data.profileId,
+            name: data.name,
+            description: data.description_profile,
+            status: data.status,
+            profilesRole: data.resources?.includes('ROLE_PROFILES'),
+            users: data.resources?.includes('ROLE_USERS'),
+            audit: data.resources?.includes('ROLE_AUDIT')
+          });
+
+          this.profileSelected$.next(data);
+
+          return;
+        }
+
+        this.profileSelected$.next(null);
+      });
+
+    this.profileSelectorSearchService.init(id);
   }
 }
