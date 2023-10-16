0ca53f8defcd8c3c67bf26fcf76e21bf96779f74
Alan Lopez <nouminosxz@gmail.com>
2023-10-16 02:53:20 -0600

    Docs added

--- src/app/admin/services/profiles/profile-selector.service.ts
+++ src/app/admin/services/profiles/profile-selector.service.ts
@@ -2,42 +2,79 @@ import { Injectable } from '@angular/core';
 import { Profile } from '../../types/response-type-profiles';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 import { QueryService } from '../../../query/services/query.service';
-import { ProfileSelectorSearchService } from './profile-selector-search.service';
+import { ProfileGetByIdService } from './profile-get-by-id.service';
 
+/**
+ * ## ProfileSelectorService handles the state of the current selected profile.
+ * @description Provides in Root of the application.
+ * @class ProfileSelectorService
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileSelectorService {
+  /**
+   * profileSelected$ is a BehaviorSubject that holds the current selected profile
+   * any subscriber will be notified of the changes.
+   * @description Used to handle the state of the selected profile.
+   * @private
+   */
   private profileSelected$ = new BehaviorSubject<Profile | null>(null);
 
+  /**
+   * Injects the dependencies needed for this service.
+   * @param queryService Used to handle the query params.
+   * @param profileGetByIdService Used to retrieve the profile from the server.
+   * @memberof ProfileSelectorService
+   */
   constructor(
     private queryService: QueryService,
-    private profileSelectorSearchService: ProfileSelectorSearchService
+    private profileGetByIdService: ProfileGetByIdService
   ) {}
 
+  /**
+   * This method is used to retrieve the profileSelected$ BehaviorSubject.
+   * @description It is meant to be called from the component consuming this service,
+   * Returns an Observable of the profileSelected$ BehaviorSubject to be subscribed.
+   * @returns Observable
+   * @memberof ProfileSelectorService
+   */
   getProfile(): Observable<Profile | null> {
     return this.profileSelected$;
   }
 
+  /**
+   * This method is used to clear the profileSelected$ BehaviorSubject.
+   * @description It is meant to be called from the component consuming this service to clear the state.
+   * @memberof ProfileSelectorService
+   */
   clearProfile() {
     this.profileSelected$.next(null);
   }
 
+  /**
+   * This method is used to set the profileSelected$ BehaviorSubject state with the profile retrieved from the server.
+   * @description It is meant to be called from the component consuming this service to set the state
+   * of the current selected profile.
+   * uses the profileGetByIdService to retrieve the profile from the server.
+   * @param id The id of the profile to be retrieved.
+   * @memberof ProfileSelectorService
+   */
   setSelectedProfile(id: number) {
     if (!id) {
       return;
     }
 
-    this.profileSelectorSearchService
+    this.profileGetByIdService
       .getProfile()
       .pipe(
         map(res => {
           if ('error' in res) {
-            console.log('error', res);
+            return null;
           }
 
           if ('loading' in res) {
-            console.log('loading', res);
+            return null;
           }
 
           return res as Profile | null;
@@ -63,6 +100,6 @@ export class ProfileSelectorService {
         this.profileSelected$.next(null);
       });
 
-    this.profileSelectorSearchService.init(id);
+    this.profileGetByIdService.init(id);
   }
 }
