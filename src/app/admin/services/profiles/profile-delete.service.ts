ca37076030ab3b88b7a993ec09beac10538a8c10
Alan Lopez <nouminosxz@gmail.com>
2023-10-15 23:35:40 -0600

    Added profile-delete.service.ts docs

--- src/app/admin/services/profiles/profile-delete.service.ts
+++ src/app/admin/services/profiles/profile-delete.service.ts
@@ -10,14 +10,33 @@ import { MessageService } from 'primeng/api';
 import { ProfileRequestService } from './profile-request.service';
 import { ProfileSelectorService } from './profile-selector.service';
 
+/**
+ * ProfileDeleteService handles the deletion request of a profile.
+ * @class ProfileDeleteService
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileDeleteService {
-  response$ = new BehaviorSubject<null | ResponseError | ResponseLoading>({
+  /**
+   * response$ is a BehaviorSubject that holds the response of the request
+   * any subscriber will be notified of the changes.
+   * @private
+   */
+  private response$ = new BehaviorSubject<
+    null | ResponseError | ResponseLoading
+  >({
     loading: true
   });
 
+  /**
+   * Injects the dependencies needed for this service.
+   *
+   * @param http Used to make the http request.
+   * @param messageService Used to display messages, provided by PrimeNg UI library.
+   * @param profileRequestService Used to refresh the profile list.
+   * @param profileSelectorService Used to clear the selected profile.
+   */
   constructor(
     private http: HttpClient,
     private messageService: MessageService,
@@ -25,6 +44,13 @@ export class ProfileDeleteService {
     private profileSelectorService: ProfileSelectorService
   ) {}
 
+  /**
+   * This method is used to call the init method to make the http request for deleting the profile
+   * is meant to be called from the component consuming this service.
+   * @param id The id of the profile to be deleted.
+   * @public
+   * @memberof ProfileDeleteService
+   */
   deleteProfile(id: number) {
     const url = new URL(`${environment.SERVER_PATH.PROFILES}/{id}`);
 
@@ -33,17 +59,27 @@ export class ProfileDeleteService {
     this.init(url.toString());
   }
 
+  /**
+   * This method is used to handle the http request for deleting the profile and
+   * set the state of the response$ BehaviorSubject.
+   * It also handles the error response and displays a message to the user.
+   * @param url The url to make the http request.
+   * @private
+   * @memberof ProfileDeleteService
+   */
   private init(url: string) {
     this.http
       .delete(url)
       .pipe(
         catchError(err => {
+          // If the server returns a ResponseError object, we return it.
           if ('error' in err) {
             const errorObj = err.error;
 
             return of(errorObj);
           }
 
+          // If the server fails in returning a ResponseError object, we create one.
           const data: ResponseError = {
             error: {
               errorCode: 0,
@@ -52,14 +88,15 @@ export class ProfileDeleteService {
               description: 'Failed to retrieve data'
             }
           };
-
           return of(data);
         })
       )
       .subscribe(data => {
+        // If the server returns a ResponseError object, we set the response$ BehaviorSubject
         if (data && 'error' in data) {
           this.response$.next(data);
 
+          // We display a message to the user.
           this.messageService.add({
             severity: 'error',
             summary: 'Error',
