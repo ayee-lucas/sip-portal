5b3f76474fb01a01fea9d12cf199891d5ea676fc
Alan Lopez <nouminosxz@gmail.com>
2023-10-27 00:22:52 -0600

    feat: design improvements

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -7,12 +7,18 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
+  mobileMenuVisible = false;
+
   constructor(
     private authStatusService: AuthStatusService,
     private TranslateService: TranslateService
   ) {
     this.TranslateService.setDefaultLang('es');
     this.TranslateService.use('es');
+  }
+
+  toggleSidebar() {
+    this.mobileMenuVisible = !this.mobileMenuVisible;
   }
 
   logout() {
