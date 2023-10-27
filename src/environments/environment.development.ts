a03823d6de39152945588937ac9b8841db3960f9
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2023-10-27 00:12:48 -0600

    resolved conflicts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,7 @@ const API_PREFIX = '/api';
 const USER_PREFIX = '/user';
 const PROFILE_PREFIX = '/profile';
 const AUDIT_PREFIX = '/audit';
+const PARKING_PREFIX = '/parking';
 
 export const environment = {
   ROUTE: {
@@ -15,7 +16,8 @@ export const environment = {
   SERVER_PATH: {
     USERS: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + USER_PREFIX,
     PROFILES: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + PROFILE_PREFIX,
-    AUDIT: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + AUDIT_PREFIX
+    AUDIT: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + AUDIT_PREFIX,
+    PARKING: ROOT_PATH + PORTAL_PREFIX + API_PREFIX + PARKING_PREFIX
   },
   refTranslate: './assets/i18n/'
 };
